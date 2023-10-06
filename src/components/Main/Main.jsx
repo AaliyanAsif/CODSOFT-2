0d129e394343ead7291ac25a0bb1660e149b01d8
AaliyanAsif <aaliyan876@gmail.com>
2023-10-06 23:11:55 +0500

    Revised Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,27 +1,27 @@
 import Intro from "./Intro/Intro";
 import "./Main.css";
+import Skill from "./Skill/Skill";
 
 export default function Main() {
   return (
     <div className="main-body">
       <div className="flex-item">
-        <h2>Expertise</h2>
+        <h1>Expertise</h1>
         <div className="container">
           <p className="typed">Front End Web Development</p>
         </div>
       </div>
       <Intro />
       <div className="flex-item">
-        <h2>Skills</h2>
+        <h1>Skills</h1>
         <ul className="skill-list">
-          <li>
-            React{" "}
-            <img src="/assests/1174949_js_react js_logo_react_react native_icon.png " />
-          </li>
-          <li>HTML5</li>
-          <li>CSS3</li>
-          <li>JavaScript</li>
-          <li>NodeJS</li>
+          <Skill
+            skillName="React"
+            skillImg="/assests/1174949_js_react js_logo_react_react native_icon.png"
+          />
+          <Skill skillName="JavaScript" skillImg="/assests/javascript.png" />
+          <Skill skillName="CSS 3" skillImg="/assests/css.png" />
+          <Skill skillName="HTML 5" skillImg="/assests/html.svg" />
         </ul>
       </div>
     </div>
