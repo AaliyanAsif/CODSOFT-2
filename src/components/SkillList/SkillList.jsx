89a4127707f931ea12cd480542984d096f993d29
AaliyanAsif <aaliyan876@gmail.com>
2023-10-09 20:46:20 +0500

    update image name "react.png"

--- src/components/SkillList/SkillList.jsx
+++ src/components/SkillList/SkillList.jsx
@@ -3,10 +3,7 @@ import Skill from "../skill/Skill";
 export default function SkillList() {
   return (
     <ul className="skill-list">
-      <Skill
-        skillName="React"
-        skillImg="./1174949_js_react js_logo_react_react native_icon.png"
-      />
+      <Skill skillName="React" skillImg="./react.png" />
       <Skill skillName="JavaScript" skillImg="./javascript.png" />
       <Skill skillName="CSS 3" skillImg="./css.png" />
       <Skill skillName="HTML 5" skillImg="./html.svg" />
